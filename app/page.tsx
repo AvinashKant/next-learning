4af48c5e3030c5ab262084afa5c855d7d7eee51a
Avinash Kant <=developer.avinashkantse@gmail.com>
2025-03-07 15:46:04 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-center top-10">
+        Start learning next
+      </div>
+    </>
+  );
+}
